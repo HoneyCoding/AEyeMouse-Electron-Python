21b35e1160887e4c8557edf88b5b2466d45ba2b3
HoneyCoding <dd3557@naver.com>
2022-11-17 20:38:54 +0900

    feat: filePath.js에 scroll과 scrollCloseButton, resources 폴더 경로 추가

--- src/common/filePath.js
+++ src/common/filePath.js
@@ -1,16 +1,19 @@
-const path = require('path');
+const path = require("path");
 
 const rootPath = path.join(process.env.PWD);
 
-const srcPath = path.join(rootPath, 'src');
-const pythonPath = path.join(rootPath, 'python');
+const srcPath = path.join(rootPath, "src");
+const pythonPath = path.join(rootPath, "python");
 
-const browserPath = path.join(srcPath, 'browser');
+const browserPath = path.join(srcPath, "browser");
 const commonPath = path.join(srcPath, "common");
 const rendererPath = path.join(srcPath, "renderer");
+const resourcesPath = path.join(srcPath, "resources");
 
-const rendererMainPath = path.join(rendererPath, 'main');
+const rendererMainPath = path.join(rendererPath, "main");
 const rendererDragPath = path.join(rendererPath, "drag");
+const rendererScrollPath = path.join(rendererPath, 'scroll');
+const rendererScrollCloseButtonPath = path.join(rendererPath, "scroll-close-button");
 
 module.exports = {
     rootPath,
@@ -20,5 +23,7 @@ module.exports = {
     commonPath,
     rendererPath,
     rendererMainPath,
-    rendererDragPath
+    rendererDragPath,
+    rendererScrollPath,
+    rendererScrollCloseButtonPath
 };
