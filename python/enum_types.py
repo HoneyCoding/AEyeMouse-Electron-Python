49653a550404df2f005faa96101f4d9162421462
HoneyCoding <dd3557@naver.com>
2022-11-20 00:25:53 +0900

    refactor: python enum_types.py 파일 내용 수정

--- python/enum_types.py
+++ python/enum_types.py
@@ -1,11 +1,11 @@
 from enum import IntEnum
 
 # Mode는 눈을 깜빡였을 때 마우스 좌클릭, 마우스 우클릭, 드래그, 스크롤, 커서 텔레포트가 되도록 하는 기능을 상징합니다.
-# Mode.LEFT_CLICK과 같은 형태로 코드를 작성해 값을 사용할 수 있습니다.
-# Mode.LEFT_CLICK.to_string()과 같이 메서드를 실행하면 "LEFT_CLICK"과 같은 str 타입이 return됩니다.
+# Mode.DOUBLE_CLICK과 같은 형태로 코드를 작성해 값을 사용할 수 있습니다.
+# Mode.DOUBLE_CLICK.to_string()과 같이 메서드를 실행하면 "DOUBLE_CLICK"과 같은 str 타입이 return됩니다.
 
 class Mode(IntEnum):
-    LEFT_CLICK = 1
+    DOUBLE_CLICK = 1
     RIGHT_CLICK = 2
     DRAG = 3
     SCROLL = 4
@@ -13,7 +13,7 @@ class Mode(IntEnum):
     
     def to_string(self):
         if self == 1:
-            return "LEFT_CLICK"
+            return "DOUBLE_CLICK"
         elif self == 2:
             return "RIGHT_CLICK"
         elif self == 3:
