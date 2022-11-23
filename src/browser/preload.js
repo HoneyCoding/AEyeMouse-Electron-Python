db59cb3e9317e6ea91ef7131ba35435748ee479c
HoneyCoding <dd3557@naver.com>
2022-11-23 17:36:26 +0900

    refactor: preload.js와 keys.js의 불필요한 코드 제거

--- src/browser/preload.js
+++ src/browser/preload.js
@@ -3,26 +3,6 @@ const { contextBridge, ipcRenderer } = require("electron");
 const Keys = require("../common/keys");
 const functions = require("../common/functions");
 
-function convertToCamelCase(key) {
-    return key
-        .split("-")
-        .map((word, index) => {
-            if (index == 0) return word.toLowerCase();
-            return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-        })
-        .join("");
-}
-
-/**
- * JSON 타입
- */
-// const api = Object.values(Keys.setMode).reduce((prevJson, keyJson) => {
-//     const newJson = { ...prevJson };
-//     const functionName = convertToCamelCase(keyJson.key);
-//     newJson[functionName] = () => ipcRenderer.send(keyJson.js);
-//     return newJson;
-// }, {});
-
 contextBridge.exposeInMainWorld("electronAPI", {
     // Set Mode Functions
     setModeDoubleClick() {
--- src/common/keys.js
+++ src/common/keys.js
@@ -1,20 +1,3 @@
-/** 해당 함수 실행 시 결과로 JSON이 return된다.
- *
- * 예를 들어 createKeyJson('click-left-mouse') 함수를 실행하면 JSON 타입인
- * { js: 'click-left-mouse-js', python: 'click-left-mouse-python' }가 return된다.
- *
- * 예를 들어 createKeyJson('click-left-mouse', ['js', 'C', 'C++']) 함수를 실행하면 JSON 타입인
- * { js: 'click-left-mouse-js', C: 'click-left-mouse-C', C++: 'click-left-mouse-C++' }가 return된다. */
-function createKeyJson(key, languages = ["js", "python"]) {
-    const keyJson = languages.reduce((prevJSON, item) => {
-        const newJSON = { ...prevJSON };
-        newJSON[item] = `${key}-${item}`;
-        return newJSON;
-    }, {});
-    keyJson["key"] = key;
-    return keyJson;
-}
-
 // Set Mode
 const setModeDoubleClick = "set-mode-double-click";
 const setModeRightClick = "set-mode-right-click";
