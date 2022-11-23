1f1cacad1a4832228c8ce5c9cd77ed95e6e57d32
HoneyCoding <dd3557@naver.com>
2022-11-23 17:03:08 +0900

    refactor: drag-scroll 파일을 control_mouse.py로 이름 변경

--- src/common/runPython.js
+++ src/common/runPython.js
@@ -3,24 +3,30 @@ const path = require("path");
 
 const filePath = require("./filePath");
 
-const runPython = () => new Promise((resolve, reject) => {
-    const python = spawn("python", [
-        path.join(filePath.pythonPath, "drag-scroll.py"),
-    ]);
+const createRunPythonPromise = (fileName) =>
+    new Promise((resolve, reject) => {
+        const python = spawn("python", [
+            path.join(filePath.pythonPath, fileName),
+        ]);
 
-    python.stdout.on("data", (data) => {
-        const dataToString = `python: ${data.toString()}`;
-        resolve(dataToString);
+        python.stdout.on("data", (data) => {
+            const dataToString = `python: ${data.toString()}`;
+            resolve(dataToString);
+        });
+
+        python.stderr.on("data", (data) => {
+            const dataToString = `python: ${data.toString()}`;
+            reject(dataToString);
+        });
+
+        python.on("close", (code) => {
+            console.log(`child process close all stdio with code ${code}`);
+        });
     });
 
-    python.stderr.on("data", (data) => {
-        const dataToString = `python: ${data.toString()}`;
-        reject(dataToString);
-    });
-
-    python.on("close", (code) => {
-        console.log(`child process close all stdio with code ${code}`);
-    });
-});
+const runPython = () =>
+    createRunPythonPromise("control_mouse.py").then(() =>
+        createRunPythonPromise("keyboard_control.py")
+    );
 
 module.exports = runPython;
