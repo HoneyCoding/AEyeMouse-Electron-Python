57489c92b1b2f6b134044fc31d8916df3a1a54cf
HoneyCoding <dd3557@naver.com>
2022-12-06 13:42:36 +0900

    feat: Scroll Buttons Window에 Move 버튼 추가

--- src/renderer/scroll-buttons/renderer.js
+++ src/renderer/scroll-buttons/renderer.js
@@ -2,8 +2,12 @@
 const mainContainer = document.getElementById("main-container");
 
 // Scroll Container Elements
+const moveButton = document.getElementById("move-btn");
 const closeButton = document.getElementById("close-btn");
 
+moveButton.addEventListener('click', function (e) {
+    electronAPI.showMoveScrollWindow();
+});
 closeButton.addEventListener("click", function (e) {
     electronAPI.hideScrollWindow();
 });
